c0e07ab2d930a6777a36f0b6a3d2bfc3bf36eeb3
Evgeny Sapozhkov <sapozhkovevgenii@gmail.com>
2024-09-10 21:23:23 +0300

    Added user model. Created async db session.

---
+++ db_handler/db_class.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from db_models.user import Base
+
+# DB_URL=postgresql+asyncpg://login:password@localhost:5432/db_name
+
+# engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
+
+engine = create_async_engine(os.getenv('DB_URL'), echo=True)  # type: ignore
+
+session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
---
+++ db_models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import DateTime, Float, String, Text, func, BigInteger
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    telegram_id = mapped_column(BigInteger)
